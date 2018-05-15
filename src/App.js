8780112263a3bd1a725be990d7121919f2050f20
Илья <sviridov9199@gmail.com>
2018-05-15 22:56:45 +0300

    Clean-up project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './Welcome';
-import Clock from './Clock';
-import List from './List';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <hr/>
-          <Welcome name='World'>world</Welcome>
-          <hr/>
-          <Clock/>
-          <hr/>
-          <List/>
+      <div>
+        <h1>Chat</h1>
       </div>
     );
   }
